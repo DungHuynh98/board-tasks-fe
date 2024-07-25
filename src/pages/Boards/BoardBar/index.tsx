2adcfddf7a9be6248a5b982519ad52b29722c395
Dung Huynh <dunghuynh.010598@gmail.com>
2024-07-25 22:15:57 +0700

    Update avatar group styles

--- src/pages/Boards/BoardBar/index.tsx
+++ src/pages/Boards/BoardBar/index.tsx
@@ -97,6 +97,12 @@ export const BoardBar = () => {
               width: 34,
               height: 34,
               fontSize: 16,
+              border: "none",
+              color: "white",
+              cursor: "pointer",
+              "&:first-of-type": {
+                bgcolor: "#a4b0be",
+              },
             },
           }}
         >
